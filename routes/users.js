ebb79f217e1d935a62159b39fa3d86e15f7a1769
zhaochangjiang <zhaochangjiang@bjvca.com>
2019-01-11 10:45:04 +0800

    2

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const router = require('koa-router')()
 
 
 const CONF = require('../conf/baseConf')
+var request = require("request");
 
 
 const prefixStr = "wx"
@@ -17,14 +18,50 @@ router.prefix(`/${prefixStr}`)
 
 router.get('/login', async (ctx, next) => {
 	
-	
+	let parm = ctx.request.query
+
+    console.log(parm);
 	console.log("login");
 	ctx.body = CONF
 	
 	//1.从服务器拿到用户的
+
+    var options = { method: 'GET',
+        url: CONF.weixinURL,
+        qs:
+            { appid: CONF.appid,
+				secret: CONF.secret,
+				js_code: parm.code,
+				grant_type: 'authorization_code' },
+		};
+
+    
 	
-	//https://api.weixin.qq.com/sns/jscode2session?appid=<AppId>&secret=<AppSecret>&js_code=<code>&grant_type=authorization_code
+	try{
+
+
+        let res = await wxLogin(options)
+
+        ctx.body ={code:200,msg:"success",data:JSON.parse(res)}
+
+    }catch (err) {
+        return ctx.body ={code:400,msg:err.message}
+    }
 	
+
+
+    async function wxLogin(options){
+        return new Promise(function(resolve,reject){
+            request(options, function (error, response, body) {
+
+                if (error) reject(error)
+                
+                resolve(body)
+            });
+        })
+    }
+
+
 })
 
 router.get('/string', async (ctx, next) => {
@@ -38,3 +75,5 @@ router.get('/json', async (ctx, next) => {
 })
 
 module.exports = router
+
+
